264f1e068bc3e56ac6303dc84c58af0d1dc07ed3
pedroAlay19 <e1312731894@live.uleam.edu.ec>
2025-12-19 18:39:43 -0500

    fix

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,15 +6,15 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   
-  app.enableCors({
-    origin: [
-      'https://studymate-frontend-app-cgh9hge2csdbhkeb.canadacentral-01.azurewebsites.net',
-      'http://localhost:5173',
-    ],
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    allowedHeaders: 'Content-Type, Authorization',
-    credentials: true,
-  });
+  // app.enableCors({
+  //   origin: [
+  //     'https://studymate-frontend-app-cgh9hge2csdbhkeb.canadacentral-01.azurewebsites.net',
+  //     'http://localhost:5173',
+  //   ],
+  //   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+  //   allowedHeaders: 'Content-Type, Authorization',
+  //   credentials: true,
+  // });
   
 
   app.useGlobalPipes(new ValidationPipe());
